f4983ace7b89727b52d758914fefe3228f8f02ed
Shresth Rajput <rajshresth2@gmail.com>
2024-08-06 16:20:12 +0530

    Lecture on Literals

---
+++ Javascript/Day-3 Tokens/literals.js
@@ -0,0 +1,19 @@
+/*const a=10;
+const b=1.35;
+const c=10n;
+console.log(typeof(a),a)
+console.log(typeof(b),b)
+console.log(typeof(c),c)*/
+
+
+/*
+const userName=prompt("Enter Your Name")
+const greet=`Good Morning ${userName}`
+console.log(greet) */
+
+const a=Symbol("Hey")
+const b=Symbol("Hey")
+console.log(a===b)
+console.log(a.description===b.description)
+
+
